3fa28ebba1dd9fe8ede96518f737d8c64d97c18f
ultragorira <lorisdeluca1988@gmail.com>
2021-06-07 09:36:16 +0300

    first commit

---
+++ base/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Results(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    search_query =  models.CharField(max_length=200)
+    link_query = models.URLField(max_length=400)
+    user_answer = models.CharField(max_length=50)
+    correct_answer = models.CharField(max_length=50)
+    verdict = models.CharField(max_length=50)
+
+
+
+
+
+# Create your models here.
