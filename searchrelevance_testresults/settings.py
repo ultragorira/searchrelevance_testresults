b06a73e1653ffb6e0d85d6b8cf9dd32e70132f84
ultragorira <lorisdeluca1988@gmail.com>
2021-06-08 18:32:12 +0300

    Edits in the login

--- searchrelevance_testresults/settings.py
+++ searchrelevance_testresults/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -114,8 +115,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGIN_URL = 'login'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
